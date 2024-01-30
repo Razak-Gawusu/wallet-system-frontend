55d6180eb2a7d6f2634c41f4629dc6ae3c2100f8
Raphael Anaafi <raphaelanaafi5@gmail.com>
2024-01-30 13:47:36 +0000

    search component

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,5 +2,6 @@ import EyeIcon from './eye.svg';
 import EyeOffIcon from './eye-off.svg';
 import FilterIcon from './filter.svg';
 import PlusIcon from './plus.svg';
+import SearchIcon from './search.svg';
 
-export { EyeIcon, EyeOffIcon, PlusIcon, FilterIcon };
+export { EyeIcon, EyeOffIcon, PlusIcon, FilterIcon, SearchIcon };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 
-import { FilterIcon } from '../assets';
-import { Button, Input } from '../components';
+import { Button, Input, Search } from '../components';
 
 export function Home() {
   const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState<string>('');
+
+  const handleSearch = (search: string) => {
+    setSearch(search);
+  };
 
   const handleLoader = () => {
     setIsLoading(true);
@@ -14,15 +18,11 @@ export function Home() {
     <div>
       <div>Home</div>
       <Input label="Name" />
-      <Button
-        isLoading={isLoading}
-        onClick={handleLoader}
-        // leftIcon={<PlusIcon />}
-        variant="tertiary"
-        rightIcon={<FilterIcon />}
-      >
+      <Button isLoading={isLoading} onClick={handleLoader} variant="tertiary">
         Filter
       </Button>
+      <Search className="w-[400px]" onSearch={handleSearch} />
+      <div>Search Value: {search}</div>
     </div>
   );
 }
