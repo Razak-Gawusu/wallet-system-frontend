acd372cdbe154e23a8df5f765452a32975385a94
Raphael Anaafi <raphaelanaafi5@gmail.com>
2024-01-30 08:17:26 +0000

    button component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,44 @@
-export function Input() {
-  return <input />;
+import React from 'react';
+
+import { EyeIcon, EyeOffIcon } from '../../assets/icons';
+import { cn } from '../../libs';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+  rightIcon?: React.ReactNode;
 }
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, type, className, ...props }, ref) => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    return (
+      <div className={cn('w-[384px]', className)}>
+        <label htmlFor={label}></label>
+        <div
+          className={cn(
+            'w-full py-3 px-4 flex justify-between items-center gap-4 border rounded',
+            {
+              'border-red-600': error,
+            },
+          )}
+        >
+          <input
+            type={showPassword ? 'text' : type}
+            className="border-none focus:outline-none"
+            {...props}
+            ref={ref}
+          />
+          {type === 'password' && (
+            <button onClick={() => setShowPassword((prev) => !prev)}>
+              {showPassword ? <EyeIcon /> : <EyeOffIcon />}
+            </button>
+          )}
+        </div>
+        {error && <div className="text-red-600 text-xs">{error}</div>}
+      </div>
+    );
+  },
+);
+
+//falsy : '', null, undefined, 0, false,
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+
+import { FilterIcon } from '../assets';
+import { Button, Input } from '../components';
+
 export function Home() {
-  return <div>Home</div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLoader = () => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div>
+      <div>Home</div>
+      <Input label="Name" />
+      <Button
+        isLoading={isLoading}
+        onClick={handleLoader}
+        // leftIcon={<PlusIcon />}
+        variant="tertiary"
+        rightIcon={<FilterIcon />}
+      >
+        Filter
+      </Button>
+    </div>
+  );
 }
